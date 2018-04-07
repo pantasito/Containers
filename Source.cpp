a9f9a12efdd08e671e71e195c6a61a85378d8ef0
Derga <pante-sergey@yandex.ru>
2018-04-07 20:14:24 +0300

    Pop function fixed

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Test.h"
-
+#include <assert.h>
 
 using namespace std;
 
@@ -42,6 +42,7 @@ class arr_queue : public a_queue<T>
           data = new T[array_length*2];
           //memcpy(data, old_data, (array_length / 2.0) * sizeof(T));
           for (int i = 0; i < array_length; i++) data[i] = old_data[i];
+          delete[] old_data;
           array_length *= 2;         
         }
         
@@ -61,39 +62,48 @@ class arr_queue : public a_queue<T>
 		} 
 
 	// Pick up first in line
-    T pop() {
-   	  if (last_ind == 0 & array_length != 0) {
+    T pop() {		
+   	  if (last_ind == 0 && array_length != 0) {
 	    T result = data[0];
+	    delete[] data;
 	    data = NULL;
 	    array_length = 0;
 	    return result;
 	  } else {
+		assert(array_length > 0);
 		if (last_ind > 0) {
 		  T result = data[0];
-	      data =& data[1];
-	      last_ind -= 1;
+		  for (int i = 0; i < last_ind; ++i) {
+			  data[i] = data[i+1];
+		  }
+		  last_ind -= 1; 
 	      return result;
-		}
+		} else {
+		  assert(false);
+		}   
       }
 	}
-    
+}    
 	// Clear the queue
     void clear() {
+		if (data != NULL)
+			delete[] data;
 		data = NULL;
 		last_ind = 0;
         array_length = 0;
-		} 
+	} 
 
 	// Container size
     size_t size() {
-		return last_ind + 1;
-		}
+		if (array_length>0) return last_ind+1;
+		return 0;
+	}
 
 	// If the container is empty
     bool empty() {
-		if (array_length == 0) return true;
+		if (data == NULL || array_length == 0) return true;
 		else return false;
-		}
+	}
 };
 
 int main()
@@ -102,12 +112,15 @@ int main()
   stl_queue<int> stl_q;
   arr_queue<int> arr_q;
   Test_int_queue test;
-  test.log_levels[1] = 1;
+  test.log_levels[1] = 0;
   test.log_levels[2] = 0;
   test.log_levels[3] = 0;
   test.add_queue_to_test(stl_q,"STL");
   test.add_queue_to_test(arr_q,"ARR");
-  test.run_all_tests();
+  test.run_all_tests(10000);
+  arr_q.clear();
+
+  
 
  // f<int>(3);
 //  f<string>("Hell");
