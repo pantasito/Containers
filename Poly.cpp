abc3e8c3ad373ed448ba04f77d4cf645cd5e337f
pantasito <pante-sergey@yandex.ru>
2018-05-16 10:01:42 +0300

    task complete

--- Poly.cpp
+++ Poly.cpp
@@ -1,4 +1,24 @@
+/*
+1) Добавить класс прямоугольник
+
+2) Реализовать: итерационный процесс, N задается
+
+У тебя есть список фигур, изначально он пустой. Задача поддерживать список упорядоченный по площадям.
+
+list<Shape*>
+
+На каждой итерации:
+Генерируется случайная фигура. Для этой фигуры генерируются случайные параметры.
+И она добавляется в список сразу в нужное место.
+Можешь принтовать площади.
+*/
+
+
+
 #include <iostream>
+#include <list>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 class Shape
@@ -8,7 +28,7 @@ class Shape
     // => класс Shape стал абстрактным
 };
 
-
+/*
 void print_max(Shape* s1, Shape* s2) {
   if (s1->area() > s2->area())
     cout << s1->area();
@@ -16,15 +36,17 @@ void print_max(Shape* s1, Shape* s2) {
     cout << s2->area();
   cout << endl;
 }
+*/
 
 class Triangle : public Shape
 {
-    int a, b, c;
+    double a, b, c;
   public:
-    Triangle(int a, int b, int c) :a(a), b(b), c(c) {}
+    Triangle(double a, double b, double c) :a(a), b(b), c(c) {}
 
     double area() {
-      return a + b + c;
+      double p = (a + b + c) / 2.0;
+      return sqrt(p * (p - a) * (p - b) * (p - c));
     }
 };
 
@@ -35,20 +57,113 @@ class Circle : public Shape
     Circle(double r) :r(r) {}
     
     double area() {
-      return acos(-1)*r*r;
+      return 3.14 * r * r;
     }
 };
 
+class Rectangle : public Shape
+{
+   double a, b;
+ public:
+   Rectangle(double a, double b) : a(a), b(b) {};
+
+   double area() {
+     return a * b;
+   }
+
+};
+
+Shape* gen() {
+  int command;
+  command = rand() % 3;
+
+  if (command == 0) {
+    double r = 0;
+    while(r == 0) {
+      r = rand() % 10;
+    }
+
+    Circle* pc = new Circle(r);
+    return pc;
+  }
+
+  if (command == 1) {
+
+    double a, b, c;
+    a = b = c = 0;
+
+    while (a == 0) {
+      a = rand() % 10;
+    }
+
+    while (b == 0) {
+      b = rand() % 10;
+    }
+
+    c = rand() % 10;
+
+    while (a + b <= c || a + c <= b || b + c <= a) {
+      c = rand() % 10;
+    }
+
+    Triangle* pt = new Triangle(a, b, c);
+    return pt;
+  }
+
+  if (command == 2) {
+    double a, b;
+    a = b = 0;
+    
+    while (a == 0) {
+      a = rand() % 10;
+    }
+
+    while (b == 0) {
+      b = rand() % 10;
+    }
+    Rectangle* pr = new Rectangle(a, b);
+    return pr;
+  }
+}
+
 int main()
 {
-  Triangle t1(1, 2, 3);
-  Triangle* pt2 = new Triangle(2, 3, 4);
-  Circle c1(2);
-  Circle* pc2 = new Circle(3);
+  int N;
+  cout << "int N = ";
+  cin >> N;
 
-  print_max(&t1, pt2);
-  print_max(&t1, pc2);
+  list <Shape*> l;
+  srand(time(0));
 
-  cout << "\n";
+  Shape* p = gen();
+  l.push_back(p);
+  N--;
+  cout << p->area() << endl << "----------------------" << endl;
+  list <Shape*>::iterator it = l.begin();
+
+
+  while (N > 0) {
+    N--;
+
+    Shape* p = gen();
+
+    for (it = l.begin(); it != l.end(); it++) {
+
+      if ((*it)->area() > p->area()) {
+        l.insert(it, p);
+        break;
+      }
+    }
+
+    if (it == l.end()) {
+      l.push_back(p);
+    }
+
+    for (auto i : l) {
+      cout << i->area() << endl;
+    }
+    cout << "----------------------" << endl;
+  }
+  
   system("pause");
 }
