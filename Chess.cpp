0e3c82a21ae259ce75f5ac7386db5c232727e6b5
pantasito <pante-sergey@yandex.ru>
2018-06-03 16:56:18 +0300

    Add print() and turn()

--- Chess.cpp
+++ Chess.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 /*
@@ -45,12 +46,23 @@ struct Position
   int x;
   int y;
   Position(int x, int y):x(x),y(y) {}
+  int get_x() { return x; }
+  int get_y() { return y; }
+
+public:
+  void set_x(int _x) { x = _x; }
+  void set_y(int _y) { y = _y; }
+  bool operator == (Position& lhs, Position& rhs) {
+    if (lhs.get_x() == rhs.get_x() && lhs.get_y() = rhs.get_x()) return true;
+    else return false;
+  }
 };
 
 class ChessParty;
 
 class Figure
-{       
+{   
+    friend ChessParty;
     Position pos;
     bool is_white;
   public:
@@ -59,22 +71,84 @@ class Figure
     virtual vector<Position> get_avail_turns(ChessParty& party) = 0;
 };
 
+
 class ChessParty
-{ 
+{
   friend Figure;
-  bool cur_white;
+  bool cur_white = true;
 
   vector<Figure*> white_figures;
   vector<Figure*> black_figures;
   
   Figure* field[8][8];
 
+  Figure* get_fig() {
+    int size = (cur_white == 1) ? white_figures.size() : black_figures.size();
+    if (size == 0) return;
+    Figure * p = (cur_white == 1) ? white_figures[rand() % size] : black_figures[rand() % size];
+    return p;
+  }
+
+  void check_and_delete_fig(bool cur_white, Position this_turn) {
+    if (cur_white == 1) {
+      for (vector<Figure*> ::iterator it = black_figures.begin(); it != black_figures.end(); it++) {
+        if (*it.pos == this_turn) {
+          black_figures.erase(it);
+          field[this_turn.get_x()][this_turn.get_y()] = NULL;
+        }
+      }
+    }
+    else 
+      for (vector<Figure*> ::iterator it = white_figures.begin(); it != white_figures.end(); it++) {
+      if (*it.pos == this_turn) {
+        white_figures.erase(it);
+        field[this_turn.get_x()][this_turn.get_y()] = NULL;
+      }
+    }
+  }
+
   public:
     ChessParty() {}
     void turn() {
-      
+      /*
+      Метод turn:
+      Осуществляет один ход, псевдокод:
+          1) Фиксируется сторона, которая ходит
+          2) Выбирается рандомная фигура
+          3) Получаем массив всех возможных ходов для этой фигуры
+          4) Если массив пуст, goto 2
+          5) Из допустимых мест хода выбирается рандомное место, куда пойдет фигура
+          6) Если в этом месте уже есть фигура, то goto 7, иначе goto 8
+              7) Удаляем съеденную фигуру с поля и из массива фигур.
+          8) Ставим на это место текущую фигуру
+          9) Очищаем предыдущее место текущей фигуры
+          10) Меняется сторона, которая ходит
+       */
 
-      //vector<Position> avail_pos = p->get_avail_turns(*this);
+      vector<Position> avail_pos;
+      srand(time(0));
+      Figure* figure = get_fig();
+      do {
+        vector<Position> avail_pos = figure->get_avail_turns(*this);
+      } while (avail_pos.size() == 0);
+
+      Position this_turn = avail_pos[rand() % avail_pos.size()];
+
+      check_and_delete_fig(cur_white, this_turn);
+      field[figure->pos.get_x()][figure->pos.get_y()] = NULL;
+
+      figure->pos.set_x(this_turn.get_x());
+      figure->pos.set_y(this_turn.get_y());
+      cur_white = 1 - cur_white;
     }
-    void print() {}
+
+    void print() {
+      for (auto i : field) {
+        for (auto j : field) {
+          if (*j == NULL) cout << ". ";
+          else cout << ". ";
+        }
+        cout << endl;
+      }
+    }
 };
